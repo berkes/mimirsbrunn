aa9283a30042fd5108a28d3e49fcae6e5c9e577c
Bèr Kessels <ber@berk.es>
2020-06-06 15:07:13 +0200

    WIP in making a filter with distance sort

--- libs/bragi/src/routes/mod.rs
+++ libs/bragi/src/routes/mod.rs
@@ -1,6 +1,7 @@
 mod autocomplete;
 mod entry_point;
 mod feature;
+mod features;
 mod params;
 mod reverse;
 mod status;
@@ -8,5 +9,6 @@ mod status;
 pub use autocomplete::{autocomplete, post_autocomplete, JsonParams};
 pub use entry_point::entry_point;
 pub use feature::feature;
+pub use features::{features, post_features};
 pub use reverse::reverse;
 pub use status::status;
